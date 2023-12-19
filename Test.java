dd39d917fb4d07f1fcaa347caee8d0758dd24ace
Rayhan Shorker <raihanshorker56@gmail.com>
2023-12-19 22:03:26 +0600

    fixed exception

--- Test.java
+++ Test.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
 public class Test {
-public static void main(String[] args) {
-int balance = 5000;
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter Amount to withdraw");
-int withdraw = sc.nextInt();
-try {
-if(balance - withdraw < 1000) {
-throw new Exception("Balance must be grater than 1000");
-}
-else {
-balance = balance - withdraw;
-}
-}catch(Exception e) {
-e.printStackTrace();
-}
-}
+    public static void main(String[] args) {
+        int balance = 5000;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Amount to withdraw");
+        int withdraw = sc.nextInt();
+        try {
+        
+            if (withdraw < 1000) {
+               
+                throw new Exception("Balance must be grater than 1000");
+            } else {
+                balance = balance - withdraw;
+                System.out.println("Successfuly withdrawn");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
 }
